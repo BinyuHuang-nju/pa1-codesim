a6b756232acd19f9ca1ba7137e9b1edcc173b862
BinyuHuang-nju <huangbinyu000@outlook.com>
2020-12-12 17:47:01 +0800

    update

---
+++ build/CMakeFiles/codesim.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/codesim.dir/codesim.cpp.o"
+  "codesim"
+  "codesim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/codesim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
